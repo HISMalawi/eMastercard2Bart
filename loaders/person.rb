4629604506d3ca966cf9d2fc8596efcd19c69776
Walter Kaunda <walterkaunda@live.co.uk>
2020-02-18 08:08:21 +0200

    Added patients and person emastercard data loaders.

--- loaders/person.rb
+++ loaders/person.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
 
 require_relative '../logging'
+require_relative '../emastercard_db_utils'
 
-module EMastercardReaders
-  module PersonReader
+module Loaders
+  module Person
     class << self
-      def read_person(sequel, patient)
+      include EmastercardDbUtils
+
+      def load(patient)
         LOGGER.debug("Constructing person for eMastercard patient ##{patient[:patient_id]}.")
 
         person = sequel[:person].select(:gender, :birthdate, :birthdate_estimated)
                                 .first(person_id: patient[:patient_id])
 
         {
-          names: read_person_names(sequel, patient),
-          addresses: read_person_addresses(sequel, patient),
-          attributes: read_person_attributes(sequel, patient),
-          relationships: read_person_relationships(sequel, patient),
+          names: read_person_names(patient),
+          addresses: read_person_addresses(patient),
+          attributes: read_person_attributes(patient),
+          relationships: read_person_relationships(patient),
           gender: person[:gender],
           birthdate: person[:birthdate],
           birthdate_estimated: person[:birthdate_estimated]
@@ -24,14 +27,14 @@ module EMastercardReaders
 
       private
 
-      def read_person_names(sequel, patient)
+      def read_person_names(patient)
         LOGGER.debug("Reading eMastercard person names for patient ##{patient[:patient_id]}")
         sequel[:person_name].select(:given_name, :family_name, :middle_name)
                             .where(person_id: patient[:patient_id])
                             .to_a
       end
 
-      def read_person_addresses(sequel, patient)
+      def read_person_addresses(patient)
         LOGGER.debug("Reading eMastercard person addresses for patient ##{patient[:patient_id]}")
         addresses = sequel[:person_address].where(person_id: patient[:patient_id])
                                            .select(:city_village)
@@ -42,12 +45,12 @@ module EMastercardReaders
         end
       end
 
-      def read_person_attributes(_sequel, patient)
+      def read_person_attributes(patient)
         LOGGER.debug("Reading eMastercard person attributes for patient ##{patient[:patient_id]}")
         [{ person_attribute_type: 'Phone number', value: patient[:patient_phone] }]
       end
 
-      def read_person_relationships(_sequel, patient)
+      def read_person_relationships(patient)
         guardian_name = patient[:guardian_name]&.strip
         return [] if guardian_name.nil? || guardian_name.size.zero?
 
