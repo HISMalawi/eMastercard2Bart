93e3b76468aa412d412b6aef6b404933d0f5621c
Walter Kaunda <walterkaunda@live.co.uk>
2020-02-20 14:21:05 +0200

    Added hiv clinic consultation processing.

--- loaders/encounters/hiv_clinic_consultation.rb
+++ loaders/encounters/hiv_clinic_consultation.rb
@@ -4,17 +4,50 @@ module Loaders
   module Encounters
     module HivClinicConsultation
       class << self
-        def load(patient, visit); end
+        def load(patient, visit)
+          observations = [side_effects(patient, visit), on_tb_treatment(patient, visit)]
 
-        def hiv_clinic_consultation(patient, visit)
           {
-            encounter_type: 'HIV Clinic Consultation',
+            encounter_type_id: Nart::Encounters::HIV_CLINIC_CONSULTATION,
             encounter_datetime: visit[:encounter_datetime],
-            observations: []
+            observations: observations.select
           }
         end
 
-        def hiv_clinic_consultation_clinician(patient, visit_date)
+        def side_effects(_patient, visit)
+          return nil unless visit[:'side effects']
+
+          {
+            concept_id: Nart::Concepts::ART_SIDE_EFFECTS,
+            obs_datetime: visit[:encounter_datetime],
+            value_coded: Nart::Concepts::UNKNOWN,
+            children: [
+              {
+                concept_id: Nart::Concepts::UNKNOWN,
+                obs_datetime: visit[:encounter_datetime],
+                value_coded: case visit[:'side effects'].upcase
+                             when 'Y' then Nart::Concepts::YES
+                             when 'N' then Nart::Concepts::NO
+                             end,
+                value_text: 'Migrated from eMastercard 1.0'
+              }
+            ]
+          }
+        end
+
+        def on_tb_treatment(_patient, visit)
+          return nil unless visit[:tb_tatus] # tb_tatus [sic] - that's how it's named in eMastercard]
+
+          {
+            concept_id: Nart::Concepts::TB_STATUS,
+            obs_datetime: visit[:encounter_datetime],
+            value_coded: case visit[:tb_tatus]
+                         when 'Rx' then Nart::Concepts::ON_TB_TREATMENT
+                         when 'Y' then Nart::Concepts::TB_SUSPECTED
+                         when 'N' then Nart::Concepts::TB_NOT_SUSPECTED
+                         when 'C' then Nart::Concepts::TB_CONFIRMED_BUT_NOT_ON_TREATMENT
+                         end
+          }
         end
       end
     end
